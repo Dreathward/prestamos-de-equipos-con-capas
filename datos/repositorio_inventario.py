055227a344c4772336107f47b1e2c6f988e6d965
Jhon Alexander Pérez <JhonAPL14@outlook.com>
2026-09-18 13:19:33 -0500

    Implement RepositorioInventario class with methods

--- datos/repositorio_inventario.py
+++ datos/repositorio_inventario.py
@@ -1,1 +1,27 @@
+class RepositorioInventario:
+    def init(self):
+        # Inventario inicial según los datos mínimos requeridos
+        self.inventario = {
+            "Portátiles": 2,
+            "Tabletas": 1
+        }
+        self.solicitudes = []
 
+    def obtener_inventario(self):
+        # Retorna una copia para evitar que otras capas lo modifiquen directamente
+        return self.inventario.copy()
+
+    def actualizar_inventario(self, equipo, cantidad):
+        if equipo in self.inventario:
+            self.inventario[equipo] -= cantidad
+
+    def registrar_solicitud(self, estudiante, equipo, cantidad, estado):
+        self.solicitudes.append({
+            "estudiante": estudiante,
+            "equipo": equipo,
+            "cantidad": cantidad,
+            "estado": estado
+        })
+
+    def obtener_solicitudes(self):
+        return self.solicitudes
